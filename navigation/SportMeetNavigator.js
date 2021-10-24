84bc7469ce8c90f2f792bf7a571d98a043fd4dd1
RotemLibrati <rotemlibrati1@gmail.com>
2021-10-24 20:40:57 +0300

    Add tab navigation and drawer navigation

--- navigation/SportMeetNavigator.js
+++ navigation/SportMeetNavigator.js
@@ -1,16 +1,34 @@
-import MainScreen from '../screens/MainScreen';
+import StartScreen from '../screens/StartScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import MainScreen from '../screens/MainScreen';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
 
 
 const EnterNavigator = createStackNavigator({
-  MainScreen: MainScreen ,
+  StartScreen: StartScreen ,
   RegisterScreen: RegisterScreen ,
   LoginScreen: LoginScreen ,
   
 });
 
+const SportMeetTabNavigator = createBottomTabNavigator({
+  Start: {
+    screen: EnterNavigator
+  },
+  Main: {
+    screen: MainScreen
+  }
+});
 
-export default createAppContainer(EnterNavigator);
+const MainNavigation = createDrawerNavigator({
+  Main: {
+    screen: SportMeetTabNavigator
+  }
+});
+
+export default createAppContainer(MainNavigation);
