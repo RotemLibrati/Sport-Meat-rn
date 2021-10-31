6f186ac0d7af0d81627e76457245902be7e08a4b
RotemLibrati <rotemlibrati1@gmail.com>
2021-10-31 04:26:35 +0200

    Page for show all games

---
+++ components/games/AllGames.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import API from '../../ApiService';
+import Loading from '../Loading';
+import Game from './Game';
+
+const AllGames = props => {
+    const [games, setGames] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        const fetchGames = async () => {
+            const result = await axios.get(`${API.ipAddress}/games/admin`);
+            setGames(result.data);
+            setIsLoading(false);
+        };
+        fetchGames();
+    }, [])
+    return (
+        isLoading ? (<Loading />) : (
+            <View >
+            {games.games.map(game => (
+                <Game key={game.id} game={game} navigation={props.navigation} />
+            ))}
+        </View> 
+        )
+    )
+};
+
+export default AllGames;
+
