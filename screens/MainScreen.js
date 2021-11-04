7943985d88ed30eecf5286e78e40740c2cb679d5
RotemLibrati <rotemlibrati1@gmail.com>
2021-11-04 06:34:01 +0200

    add icons

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,31 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import RecentGame from "../components/games/RecentGame";
-import Teams from "../components/teams/Teams";
+import RecentTeams from "../components/teams/RecentTeams";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/HeaderButton";
 
 const MainScreen = props => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>שלום</Text>
-      <RecentGame navigation={props.navigation}/>
-      <Teams navigation={props.navigation}/>
+      <RecentGame navigation={props.navigation} />
+      <RecentTeams navigation={props.navigation} />
     </View>
   );
 };
-MainScreen.navigationOptions = () => {
+MainScreen.navigationOptions = (navData) => {
   return {
-      headerTitle: "ראשי"
+    headerTitle: "ראשי",
+    headerLeft: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+      <Item
+        title="Menu"
+        iconName="menu"
+        onPress={() => { navData.navigation.toggleDrawer() }} />
+    </HeaderButtons>
   }
 };
 const styles = StyleSheet.create({
-  container: { 
+  container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     // justifyContent: 'center',
   },
   title: {
-    fontSize:24
+    fontSize: 24
   }
 });
 
