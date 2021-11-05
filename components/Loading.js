80cb4bf6ec6fc4bda8381d2ac351ea9bea761548
RotemLibrati <rotemlibrati1@gmail.com>
2021-11-05 14:42:33 +0200

    Add a spinning wheel icon

--- components/Loading.js
+++ components/Loading.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
 
 const Loading = () => {
     return (
-        <View>
-            <Text>Loading</Text>
+        <View style={[styles.container, styles.horizontal]}>
+            <ActivityIndicator size="large" />
         </View>
     )
-}
+};
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center"
+    },
+    horizontal: {
+      flexDirection: "row",
+      justifyContent: "space-around",
+      padding: 10
+    }
+  });
 
-export default Loading
+export default Loading;
