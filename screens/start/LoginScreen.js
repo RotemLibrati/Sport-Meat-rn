69059271927a8927e7e53e0164730f75955aa1fd
RotemLibrati <rotemlibrati1@gmail.com>
2021-11-14 12:40:13 +0200

    Fix the problam of login button

--- screens/start/LoginScreen.js
+++ screens/start/LoginScreen.js
@@ -8,59 +8,58 @@ const LoginScreen = props => {
     const [tryLoggedin, setTryLoggedin] = useState(false);
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [accessToken, setAccessToken] = useState('');
-    const loginUser = () => {
+    const login = (token) => {
+        addToken(token)
+        addUsername(username);
+        loginHandler(true);
+    };
+    const notLogin = () => {
+        loginHandler(false);
+        setTryLoggedin(true);
+    }
+    const loginUser = async () => {
         let formdata = new FormData();
         formdata.append("username", username);
         formdata.append("password", password);
         formdata.append("grant_type", "password");
         formdata.append("client_id", API.clientID);
-        fetch(`${API.ipAddress}/o/token/`, {
+        await fetch(`${API.ipAddress}/o/token/`, {
             method: "POST",
             body: formdata
         })
             .then(resp => resp.json())
-            .then(function(resp){ 
-                setAccessToken(resp.access_token)
-                if (accessToken) {
-                    addToken(accessToken);
-                    addUsername(username);
-                    loginHandler(true);
-                    props.navigation.navigate("MainScreen");
-                } else
-                    setTryLoggedin(true);
+            .then(function (resp) {
+                resp.access_token ? login(resp.access_token) : notLogin()
             })
-            //.then(resp => console.log(resp))
             .catch(error => console.log(error))
-        }
-    
+    }
+
     return (
         <ScrollView keyboardShouldPersistTaps='always'>
             {/* <View style={styles.container}> */}
-                <Text>התחברות</Text>
-                {/* {tryLoggedin ? <Text>שם המשתמש או הסיסמה אינם נכונים</Text> : */}
-                    <Text>אנא הכנס שם משתמש וסיסמה</Text>
+            <Text>התחברות</Text>
+            {tryLoggedin ? <Text>שם המשתמש או הסיסמה אינם נכונים</Text> :
+            (<Text>אנא הכנס שם משתמש וסיסמה</Text>)}
 
-                <TextInput
-                    style={styles.input}
-                    placeholder="שם משתמש"
-                    onChangeText={setUsername}
-                    value={username}
-                />
-                <TextInput
-                    secureTextEntry={true}
-                    style={styles.input}
-                    placeholder="סיסמה"
-                    onChangeText={setPassword}
-                    value={password}
-                />
-                <Button title="התחבר"
-                    onPress={loginUser} />
+            <TextInput
+                style={styles.input}
+                placeholder="שם משתמש"
+                onChangeText={setUsername}
+                value={username}
+            />
+            <TextInput
+                secureTextEntry={true}
+                style={styles.input}
+                placeholder="סיסמה"
+                onChangeText={setPassword}
+                value={password}
+            />
+            <Button title="התחבר"
+                onPress={loginUser} />
             {/* </View> */}
         </ScrollView>
     );
 };
-// () => props.navigation.navigate("RegisterScreen")
 LoginScreen.navigationOptions = () => {
     return {
         headerTitle: "התחברות"
