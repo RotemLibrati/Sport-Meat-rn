e11e72b70e2742c31b93b992f99a8c4fd0416e42
RotemLibrati <rotemlibrati1@gmail.com>
2021-11-20 17:07:54 +0200

    screen for display details of message

---
+++ components/messages/DetailsMessage.js
@@ -0,0 +1,57 @@
+import React, {useContext, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { SetToken } from '../../context/SetToken';
+import ApiService from '../../ApiService';
+const DetailsMessage = props => {
+
+    const message = props.navigation.getParam("message", null);
+    const { token } = useContext(SetToken);
+    const saveMessageHandler = async () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${token}`);
+
+        var formdata = new FormData();
+        formdata.append("seen", 1);
+
+
+        var requestOptions = {
+            method: 'PUT',
+            headers: myHeaders,
+            body: formdata,
+            redirect: 'follow'
+        };
+
+        await fetch(`${ApiService.ipAddress}/message/${message.id}/`, requestOptions)
+            .then(function (response) {
+                response.json();
+            })
+            .catch(error => console.log('error', error));
+
+    };
+    useEffect(() => {
+        saveMessageHandler();
+    }, [])
+    return (
+        <View>
+            <Text style={styles.title}>{message.subject}</Text>
+            <Text style={styles.content}>{message.body}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 24,
+        textAlign: 'center',
+        alignContent: 'center',
+        
+    },
+    content: {
+        fontSize: 18,
+        textAlign: 'right',
+        alignContent: 'center'
+    }
+});
+
+export default DetailsMessage;
+;
