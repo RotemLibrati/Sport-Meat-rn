54a98d91073e0aee041c14e94d15f7e2a37c532a
RotemLibrati <rotemlibrati1@gmail.com>
2022-01-29 13:46:04 +0200

    Create option to choose type of team

--- components/teams/CreateNewTeam.js
+++ components/teams/CreateNewTeam.js
@@ -5,20 +5,26 @@ import API from '../../ApiService';
 import { SetToken } from '../../context/SetToken';
 import Data from '../../data/data'
 import SelectDropdown from 'react-native-select-dropdown';
+import { AppStyles, pageStyle, InputStyle, DropdownStyle } from '../styles/AppStyles';
+
 
 
 const CreateNewTeam = props => {
     const { token, username } = useContext(SetToken);
     const [name, setName] = useState('');
     const [sport, setSport] = useState('');
-    let data = Data.typeSport
+    const [typeTeam, setTypeTeam] = useState('');
+    let data = Data.typeSport;
+    let type = ['קבוצה פרטית','קבוצה פומבית'];
     const createTeamHandler = () => {
         let myHeaders = new Headers();
         myHeaders.append("Authorization", `Bearer ${token}`);
         let formdata = new FormData();
         formdata.append("name", name);
         formdata.append("sport", sport);
-        
+        formdata.append("type", typeTeam);
+
+
         let requestOptions = {
             method: 'POST',
             headers: myHeaders,
@@ -33,20 +39,29 @@ const CreateNewTeam = props => {
         props.navigation.goBack()
     }
     return (
-        <ScrollView style={styles.scroll}>
-            <View style={styles.container}>
-                <Text>פתיחת קבוצה חדשה</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="שם הקבוצה"
-                    onChangeText={setName}
-                    value={name}
-                />
-                <SelectDropdown
+        <ScrollView>
+            <View style={pageStyle.container}>
+                <Text style={pageStyle.title}>פתיחת קבוצה חדשה</Text>
+                <View style={InputStyle.inputContainerView}>
+                    <TextInput
+                        style={InputStyle.bodyInput}
+                        placeholder="שם הקבוצה"
+                        onChangeText={setName}
+                        value={name}
+                        placeholderTextColor={AppStyles.color.grey}
+                        underlineColorAndroid="transparent"
+                    />
+                </View>
+                <View style={InputStyle.inputContainerView}>
+                    <SelectDropdown
+                        buttonStyle={DropdownStyle.dropdownButton}
+                        buttonTextStyle={DropdownStyle.dropdownTextButton}
+                        placeholderTextColor={AppStyles.color.grey}
+                        underlineColorAndroid="transparent"
                         defaultButtonText="בחר סוג ספורט"
                         onSelect={(index) => {
                             setSport(index);
-                            
+
                         }}
                         data={data}
                         buttonTextAfterSelection={(selectedItem, index) => {
@@ -56,6 +71,27 @@ const CreateNewTeam = props => {
                             return item
                         }}
                     />
+                </View>
+                <View style={InputStyle.inputContainerView}>
+                    <SelectDropdown
+                        buttonStyle={DropdownStyle.dropdownButton}
+                        buttonTextStyle={DropdownStyle.dropdownTextButton}
+                        placeholderTextColor={AppStyles.color.grey}
+                        underlineColorAndroid="transparent"
+                        defaultButtonText="בחר סוג קבוצה"
+                        onSelect={(index) => {
+                            setTypeTeam(index);
+
+                        }}
+                        data={type}
+                        buttonTextAfterSelection={(selectedItem, index) => {
+                            return selectedItem
+                        }}
+                        rowTextForSelection={(item, index) => {
+                            return item
+                        }}
+                    />
+                </View>
             </View>
             <Button title="צור קבוצה"
                 onPress={createTeamHandler}
