ed99737b8ecc2ec71ba2069569027e5db770f85b
RotemLibrati <rotemlibrati1@gmail.com>
2022-02-20 20:53:32 +0200

    For each game the users can check who meat to arrive

---
+++ screens/games/AttendancesPlayers.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, ScrollView, FlatList } from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
+
+const AttendancesPlayers = props => {
+    const attendance = props.navigation.getParam("attendance", null);
+    const onPressUser = (item) => {
+        props.navigation.navigate("FriendsProfileScreen", { profile: item });
+    }
+    const renderAttendances = ({ item }) => (
+        <ListItem bottomDivider onPress={() => onPressUser(item.profile)}>
+            <Avatar source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar2.png' }} />
+            <ListItem.Content>
+                <ListItem.Title
+                >{item.profile.user.username}</ListItem.Title>
+                <ListItem.Subtitle
+                >{item.profile.city}</ListItem.Subtitle>
+            </ListItem.Content>
+        </ListItem>
+    )
+    return (
+        <FlatList
+            data={attendance}
+            renderItem={renderAttendances}
+            keyExtractor={(item) => item.profile.id.toString()}
+        />
+    )
+};
+
+export default AttendancesPlayers;
