bfa8043c90e64d37e30ac6f73b3a99a2bac9eea4
RotemLibrati <rotemlibrati1@gmail.com>
2022-02-20 20:54:30 +0200

    Add http request for get the users that signed "מגיע"

--- components/games/GameDetails.js
+++ components/games/GameDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Linking } from 'react-native';
 import { ButtonGroup } from 'react-native-elements/dist/buttons/ButtonGroup';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import CustomHeaderButton from '../HeaderButton';
@@ -7,16 +7,17 @@ import { SetToken } from '../../context/SetToken';
 import axios from 'axios';
 import API from '../../ApiService';
 import { AppStyles, PageStyle, InputStyle, DropdownStyle } from '../../components/styles/AppStyles';
+import Button from "react-native-button";
 
 const GameDetails = (props) => {
     const { username, token } = useContext(SetToken);
     const [selectedIndex, setSelectedIndex] = useState();
+    const [attendances, setAttendances] = useState([]);
     const component1 = () => { return (<Text>מגיע</Text>) }
     const component2 = () => { return (<Text >לא מגיע</Text>) }
     const component3 = () => { return (<Text>אולי מגיע</Text>) }
     const game = props.navigation.getParam("game", null);
     const buttons = [{ element: component1 }, { element: component2 }, { element: component3 }]
-    console.log(selectedIndex);
 
     // CREATE USEEFFECT AT TIME PLAYER GET IN TO THIS PAGE SEND POST REQUEST FOR CREATE NEW ATTENDANCE WITH NULL ATTENDANCE
     // CHECK THIS WITH ROTEM USER
@@ -35,7 +36,6 @@ const GameDetails = (props) => {
             };
             await axios(config)
                 .then(function (response) {
-                    console.log(response.data);
                     response.data.attendance.status == 'מגיע' ? setSelectedIndex(0) :
                         response.data.attendance.status == 'לא מגיע' ? setSelectedIndex(1) :
                             response.data.attendance.status == 'אולי מגיע' ? setSelectedIndex(2) :
@@ -72,7 +72,34 @@ const GameDetails = (props) => {
             .catch(error => console.log('error', error));
 
     };
-
+    useEffect(() => {
+        // props.navigation.addListener('didFocus',
+        //     payload => {
+        //         fetchAttendance();
+        //     });
+        const fetchAttendancesPlayers = async () => {
+            const auth = `Bearer ${token}`;
+            var config = {
+                method: 'get',
+                url: `${API.ipAddress}/attendance-for-game/${game.id}/`,
+                headers: {
+                    'Authorization': `${auth}`
+                }
+            };
+            await axios(config)
+                .then(function (response) {
+                    console.log(response.data);
+                    setAttendances(response.data.attendance);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        };
+        fetchAttendancesPlayers();
+    },[])
+    const AttendancesPlayerHandler = () => {
+        props.navigation.navigate("AttendancesPlayers", {'attendance': attendances});
+    }
     useEffect(() => {
         if (username) {
             props.navigation.setParams({ username: username });
@@ -94,12 +121,27 @@ const GameDetails = (props) => {
                         selectedIndex == 2 ? styles.maybeArraiving : styles.notChoose} />
                 <Text style={PageStyle.title}>פרטי משחק</Text>
                 <View style={styles.boxes}>
-                    <Text style={[PageStyle.TextStyle, { marginTop: '15%'}]}>זמן המשחק: {game.event_time}</Text>
+                    <Text style={[PageStyle.TextStyle, { marginTop: '15%' }]}>זמן המשחק: {game.event_time}</Text>
                     <Text style={PageStyle.TextStyle}>שם המגרש: {game.location.name}</Text>
                     <Text style={PageStyle.TextStyle}>מיקום המשחק: {game.location.region}</Text>
                     <Text style={PageStyle.TextStyle}>קבוצה: {game.team.name}</Text>
                 </View>
+                <View style={styles.buttonsContainer}>
+                    <Button
+                        containerStyle={styles.navigationContainer}
+                        style={styles.navigationText}
+                        onPress={() => Linking.openURL(`https://waze.com/ul?q=${game.location.street} ${game.location.address_number} ${game.location.region}`)}>
+                        נווט למקום
+                    </Button>
+                    <Button
+                        containerStyle={styles.attendanceContainer}
+                        style={styles.attendanceText}
+                        onPress={AttendancesPlayerHandler}>
+                        מי מגיע ?
+                    </Button>
+                </View>
             </View>
+
         </ScrollView>
     )
 };
@@ -125,6 +167,37 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         width: '80%'
     },
+    buttonsContainer: {
+        // display: 'flex',
+        width: '50%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginRight: '20%'
+
+    },
+    navigationContainer: {
+        width: AppStyles.buttonWidth.main,
+        backgroundColor: AppStyles.color.tint,
+        borderRadius: AppStyles.borderRadius.main,
+        padding: 10,
+        marginTop: 30,
+    },
+    navigationText: {
+        color: AppStyles.color.white,
+    },
+    attendanceContainer: {
+        width: AppStyles.buttonWidth.main,
+        backgroundColor: AppStyles.color.white,
+        borderRadius: AppStyles.borderRadius.main,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: AppStyles.color.tint,
+        marginTop: 30,
+    },
+    attendanceText: {
+        color: AppStyles.color.tint,
+    },
+
 })
 
 GameDetails.navigationOptions = (navData) => {
