728c5e12ed548888c3614308d239eda0ecbdfac5
RotemLibrati <rotemlibrati1@gmail.com>
2022-03-15 02:19:30 +0200

    Added ip address of heroku

--- ApiService.js
+++ ApiService.js
@@ -1,5 +1,7 @@
 export default{
     ipAddress: 'http://172.20.10.2:8000',
-    clientID: 'CTlckZ9iSLB8y7PxrJFHiaQwgzUH3xMFHwqQtzH7'
+    clientID: 'qgE2N13QqSPTGDNHmls0cqUKsuT7TGu6cxcd9zUe',
+    ipAddressHeroku: "https://sport-meet-sami-shamoon.herokuapp.com/",
+    clientIDHeroku: "JIhS7EGTTBY0zwn1UXmVTTZWTTq4jxs8FRnPYCqS"
 };
 
