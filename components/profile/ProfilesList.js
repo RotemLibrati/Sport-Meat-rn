f66b128013f71721de5b145a2adc7d45224c22d0
RotemLibrati <rotemlibrati1@gmail.com>
2022-03-18 14:55:58 +0200

    Create component for display list of users

---
+++ components/profile/ProfilesList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FlatList, Text, View } from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
+
+const ProfilesList = props => {
+    // const users = props.navigation.getParam("users", null);
+    const users = props.members;
+    console.log(users);
+    // const onPressUser = (item) => {
+    //     props.navigation.navigate("FriendsProfileScreen", { profile: item });
+    // }
+    const renderUsers = ({ item }) => (
+        <ListItem bottomDivider onPress={() => props.onPressMember(item)}>
+            {item.sex === 'זכר' ? <Avatar source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar2.png' }} />
+            : <Avatar source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar3.png' }} /> }
+            <ListItem.Content>
+                <ListItem.Title
+                >{item.user.username}</ListItem.Title>
+                <ListItem.Subtitle
+                >{item.city}</ListItem.Subtitle>
+            </ListItem.Content>
+        </ListItem>
+    )
+    return (
+        <FlatList
+            data={users}
+            renderItem={renderUsers}
+            keyExtractor={(item) => item.id.toString()}
+        />
+    )
+    // return (
+    //     <View><Text>rotem</Text></View>
+    // )
+};
+
+export default ProfilesList;
