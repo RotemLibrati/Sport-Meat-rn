c575437238fcf94b6643a795b717311e677ee2da
RotemLibrati <rotemlibrati1@gmail.com>
2022-03-18 14:56:24 +0200

    Using new component for display attendance

--- screens/games/AttendancesPlayers.js
+++ screens/games/AttendancesPlayers.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import { View, Text, ScrollView, FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements';
+import ProfilesList from '../../components/profile/ProfilesList';
 
 const AttendancesPlayers = props => {
     const attendance = props.navigation.getParam("attendance", null);
+    let userLists = attendance.map(item => item.profile);
     const onPressUser = (item) => {
         props.navigation.navigate("FriendsProfileScreen", { profile: item });
     }
-    const renderAttendances = ({ item }) => (
-        <ListItem bottomDivider onPress={() => onPressUser(item.profile)}>
-            <Avatar source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar2.png' }} />
-            <ListItem.Content>
-                <ListItem.Title
-                >{item.profile.user.username}</ListItem.Title>
-                <ListItem.Subtitle
-                >{item.profile.city}</ListItem.Subtitle>
-            </ListItem.Content>
-        </ListItem>
-    )
     return (
-        <FlatList
-            data={attendance}
-            renderItem={renderAttendances}
-            keyExtractor={(item) => item.profile.id.toString()}
-        />
+        <View>
+            <ProfilesList  members={userLists} onPressMember={onPressUser}/>
+        </View>
     )
 };
 AttendancesPlayers.navigationOptions = () => {
