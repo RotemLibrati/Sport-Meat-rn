50b80818755edd08436947abab9381e835e5d62d
RotemLibrati <rotemlibrati1@gmail.com>
2022-05-03 05:13:51 +0300

    Added image

--- screens/start/StartScreen.js
+++ screens/start/StartScreen.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Button from "react-native-button";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
 import { AppStyles } from "../../components/styles/AppStyles";
 
+
+const TEAM_ITEM_HEIGHT = 180;
+const TEAM_ITEM_MARGIN = 10;
+const teamNumColums = 2;
+const { width, height } = Dimensions.get("window");
+const SCREEN_WIDTH = width < height ? width : height;
 const StartScreen = props => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Sport Meet</Text>
+      <Image style={styles.photo} source={require('../../assets/pictures/SportsMeet.jpg')}/>
       <Button
         containerStyle={styles.loginContainer}
         style={styles.loginText}
@@ -66,5 +73,12 @@ const styles = StyleSheet.create({
   signupText: {
     color: AppStyles.color.tint,
   },
+  photo: {
+    width: (SCREEN_WIDTH - (teamNumColums + 1) * TEAM_ITEM_MARGIN) / teamNumColums,
+    height: TEAM_ITEM_HEIGHT,
+    borderRadius: 15,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0
+},
 });
 export default StartScreen;
